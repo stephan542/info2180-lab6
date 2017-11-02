01ef004649c3d68fed0f8a77902d0eaa8c9ef06b
Stephan <stephanwitter@yahoo.com>
2017-11-02 04:14:00 +0000

    Exercise 3:Look up any word

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 /* global $ */ 
 $(document).ready(function(){
+    
+    var displayedword = []
            $('#submit').click(function(){
-               $.ajax({
+               if(displayedword.indexOf(($('#search_box').val()).toLowerCase())>-1){
+                    alert("Word is already displayed");
+               }else{
+                   $.ajax({
                    type: "GET",
-                   data:{q:"definition"},
+                   data:{q:($('#search_box').val()).toLowerCase()},
                    url: 'request.php',
                    cache:false,
                    success:function(d){
-                       alert(d);
-                   }
+                        $("#results").append(d);
+                        $("#results").append("<hr/ width=800px>");
+                   },
                })
+               displayedword.push(($('#search_box').val()).toLowerCase());
+               }
            })
        });
