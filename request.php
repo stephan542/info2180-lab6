7d4d29939941b6e379be5f701ed457dd67eea29b
Stephan <stephanwitter@yahoo.com>
2017-11-03 10:42:28 +0000

    XML data

--- request.php
+++ request.php
@@ -1,6 +1,7 @@
 <?php
 // accept a term (keyword)
 // respond with a value
+
 $query = $_GET['q'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
@@ -11,5 +12,50 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<definitions>
+    <word>
+        <head>definition</head>
+        <content>A statement of the exact meaning of a word, especially in a dictionary.</content>
+    </word>
+    <word>
+        <head>bar</head>
+        <content>A place that sells alcholic beverages.</content>
+    </word> 
+    <word>
+        <head>ajax</head>
+        <content>Technique which involves the use of javascript and xml (or JSON).</content>
+    </word>
+    <word>
+        <head>html</head>
+        <content>The standard markup language for creating web pages and web applications.</content>
+    </word> 
+    <word>
+        <head>css</head>
+        <content>A style sheet language used for describing the presentation of a document written in a markup language.</content>
+    </word>
+    <word>
+        <head>javascript</head>
+        <content>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</content>
+    </word>
+     <word>
+        <head>php</head>
+        <content>A server-side scripting language, and a powerful tool for making dynamic and interactive websites.</content>
+    </word>
+</definitions>';
+
+if($definition[$query] != ""){
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
+if($query == ""){
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput ->asXML();
+}else{
+    print '';
+}
+
+
+
--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 /* global $ */ 
+
 $(document).ready(function(){
     
     var displayedword = []
            $('#submit').click(function(){
-               if(displayedword.indexOf(($('#search_box').val()).toLowerCase())>-1){
+               if(displayedword.indexOf(($('#search_box').val()).toLowerCase())>-1 || displayedword[0] =="all"){
                     alert("Word is already displayed");
+               }else if($('#search_box').val() === ""){
+                   alert("No value is entered");
+                   
                }else{
                    $.ajax({
                    type: "GET",
@@ -12,10 +16,10 @@ $(document).ready(function(){
                    url: 'request.php',
                    cache:false,
                    success:function(d){
-                       if(d.length > 1){
-                        $("#results").append(d);
-                        $("#results").append("<hr/ width=800px>");
-                        displayedword.push(($('#search_box').val()).toLowerCase());
+                       if(d != ''){
+                            $("#results").append(d);
+                            $("#results").append("<hr/ width=900px>");
+                            displayedword.push(($('#search_box').val()).toLowerCase());
                        }else{
                            alert("Word not in dictionary")
                        }
@@ -25,5 +29,40 @@ $(document).ready(function(){
                }
            })
            
+           
+           document.getElementById("submitall").onclick = function(){
+                var fileRequest = new XMLHttpRequest();
+
+                var url = "request.php";
+                fileRequest.onreadystatechange = getXml;
+                fileRequest.open('GET', url);
+                fileRequest.send({q:''});
+        
+                function getXml() {
+                     if (fileRequest.readyState === 4 && fileRequest.status === 200) {
+                         var getdata = fileRequest.responseXML;
+                         var dataitems = getdata.getElementsByTagName("word");
+
+                         var datalist = document.createElement("ol");
+                        $('#results').html("");
+                         document.querySelector('#results').appendChild(datalist);
+                         
+                         for(var x = 0;x<dataitems.length;x++){
+                             var items = document.createElement("li");
+                             var head = document.createElement("h3");
+                             var tail= document.createElement("p");
+                             items.appendChild(head);
+                             items.appendChild(tail);
+                             var data = document.createTextNode(dataitems[x].children[0].childNodes[0].nodeValue);
+                             head.appendChild(data);
+                             data = document.createTextNode(dataitems[x].children[1].childNodes[0].nodeValue);
+                             tail.appendChild(data);
+                             datalist.appendChild(items);
+                         }
+                         
+                          displayedword.push("all");
+                }
+              };
+           }
            
        });
